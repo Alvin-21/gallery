af2fc9fc2a0ed40dce9a4ea6736b741649d47a26
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-16 17:19:55 +0300

    Add methods for Location class.

--- photos/models.py
+++ photos/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     category_name = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.name
+        return self.category_name
 
     def save_category(self):
         self.save()
@@ -21,6 +21,19 @@ class Category(models.Model):
 class Location(models.Model):
     country = models.CharField(max_length=30)
     city = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.city + ', ' + self.country
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def update_location(cls, id, new_city, new_country):
+        cls.objects.filter(id=id).update(new_city=new_city, new_country=new_country)
 
 class Image(models.Model):
     image = models.ImageField(upload_to='pictures/', null=True)
