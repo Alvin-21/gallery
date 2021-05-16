745876c409cfd5ae530252fb080b82b67ab0d7f8
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-16 14:04:14 +0300

    Add category model.

--- photos/models.py
+++ photos/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Image(models.Model):
-    """
-    """
     image = models.ImageField(upload_to='pictures/', null=True)
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=128)
-    
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
