038896c820e96311daa14a4be56e9bf4570d20be
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-17 11:40:54 +0300

    Configure admin.py

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Location, Image
 
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('category',)
+
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Image, ImageAdmin)
