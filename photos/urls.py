5e2f343f1f9d4f076858dc40ebb0ae14c8d80ac0
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-17 11:33:58 +0300

    Fix url bug.

--- photos/urls.py
+++ photos/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    re_path(r'^$', views.homepage, name='homepage'),
+    re_path(r'^$', views.index, name='homepage'),
 ]
 
 if settings.DEBUG:
